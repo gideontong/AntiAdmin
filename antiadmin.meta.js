9c9b58f5aedf962d1d4b1b0b9192cf4bc8fa573f
Gideon Tong <gideon@gideontong.com>
2020-08-09 13:23:53 -0700

    Add update and download URLs

---
+++ antiadmin.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         AntiAdmin
+// @namespace    https://gideontong.com
+// @version      1.1
+// @description  Userscript that automatically disables permissions and redirects when adding a bot to a Discord server
+// @homepageURL  https://github.com/gideontong/AntiAdmin
+// @supportURL   https://github.com/gideontong/AntiAdmin/issues
+// @updateURL    https://github.com/gideontong/AntiAdmin/raw/master/antiadmin.meta.js
+// @downloadURL  https://github.com/gideontong/AntiAdmin/raw/master/antiadmin.user.js
+// @icon         
+// @author       Gideon Tong
+// @license      MIT
+// @match        https://discord.com/oauth2/authorize*
+// @run-at       document-start
+// @grant        none
+// @copyright    2020
+// ==/UserScript==
--- antiadmin.user.js
+++ antiadmin.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         AntiAdmin
 // @namespace    https://gideontong.com
-// @version      1.0
+// @version      1.1
 // @description  Userscript that automatically disables permissions and redirects when adding a bot to a Discord server
 // @homepageURL  https://github.com/gideontong/AntiAdmin
 // @supportURL   https://github.com/gideontong/AntiAdmin/issues
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/gideontong/AntiAdmin/raw/master/antiadmin.meta.js
+// @downloadURL  https://github.com/gideontong/AntiAdmin/raw/master/antiadmin.user.js
 // @icon         
 // @author       Gideon Tong
 // @license      MIT
