9c019b613c1a6404c9bf3d19c62f40523f7f8c36
Gideon Tong <gideon@gideontong.com>
2020-08-09 13:39:17 -0700

    Add icon to userscript

--- antiadmin.user.js
+++ antiadmin.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         AntiAdmin
 // @namespace    https://gideontong.com
-// @version      1.1
+// @version      1.2
 // @description  Userscript that automatically disables permissions and redirects when adding a bot to a Discord server
 // @homepageURL  https://github.com/gideontong/AntiAdmin
 // @supportURL   https://github.com/gideontong/AntiAdmin/issues
 // @updateURL    https://github.com/gideontong/AntiAdmin/raw/master/antiadmin.meta.js
 // @downloadURL  https://github.com/gideontong/AntiAdmin/raw/master/antiadmin.user.js
-// @icon         
+// @icon         https://raw.githubusercontent.com/gideontong/AntiAdmin/master/assets/iron_64.png
 // @author       Gideon Tong
 // @license      MIT
 // @match        https://discord.com/oauth2/authorize*
